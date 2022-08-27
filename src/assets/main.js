aafc294c4ad6fc204fd9890d7ed7d799fd38cd32
Angel Duarte <adavid.tinocod@gmail.com>
2022-08-27 01:14:45 -0500

    top rated section made :D

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,9 +11,25 @@ async function getTrendingMoviesPreview() {
         let moviePreview = `<img class="poster-container__img" src="https://image.tmdb.org/t/p/w300${movie.poster_path}" alt="${movie.title}">`;
         allMovies.push(moviePreview);
     });
-    console.log(allMovies);
     let container = document.getElementById('trending-section');
     container.innerHTML = allMovies.join('');
 }
 
+async function getTopRatedMoviesPreview() {
+    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=' + API_KEY);
+    const data = await response.json();
+
+    const allMovies = [];
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        let moviePreview = `<img class="poster-container__img" src="https://image.tmdb.org/t/p/w300${movie.poster_path}" alt="${movie.title}">`;
+        allMovies.push(moviePreview);
+    });
+    let container = document.getElementById('top-rated-section');
+    container.innerHTML = allMovies.join('');
+}
+
+
 getTrendingMoviesPreview();
+getTopRatedMoviesPreview();
